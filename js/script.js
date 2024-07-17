c4833b16ec50a94d20303542c5b8eb13c03bd3c0
Marcin.Lukasik <marcinlukasik123@gmail.com>
2024-07-17 12:20:08 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// console.log("Hello World");
+// anime({
+//     targets: '.img-profile',
+//     scale:{
+//         value:2,
+//         duration:1600,
+//         delay:800,
+//         easing:'easeInOutQuart'
+//     },
+//     delay:250
+//   });
